b2e45e75dcf6baa5b013ae345af76dee8aaa9c98
Hashed <z-donnelly@hvcc.edu>
2019-12-11 01:36:21 -0500

    implemented firebase utils, included ability to store authenticated users into firestore DB

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/shop.component';
 import HomePage from './pages/homepage/homepage.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
     constructor() {
@@ -21,16 +21,33 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        // subscriber-it will send this every time until the user logs out
+        console.log(this.state.currentUser);
+        // -subscriber-
+        // will send this every time until the user logs out
         // open messaging system between app/firebase
         // whenever any change occurs on app firebase sends out a message
         // connection is always open as long as app component is renders
         // must close to avoid memory leaks
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({
-                currentUser: user
-            });
-            console.log(user);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            // if a user logs in
+            if (userAuth) {
+                // checking if DB updated with this data or if the snapshot has changed
+                const userRef = await createUserProfileDocument(userAuth);
+                // the moment in instantiates it will send us a snapshot object
+                userRef.onSnapshot(snapShot => {
+                    // creating a new object with the properties we want
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    });
+                });
+            } else {
+                this.setState({
+                    currentUser: userAuth
+                });
+            }
         });
     }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,40 @@ const config = {
     measurementId: 'G-PPS0J9DSDC'
 };
 
+// When the user logs in if they are not already logged in
+// will create a reference in the firestore database with email/display name & timestamp
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    // if there is no user auth object exit out of this function
+    if (!userAuth) return;
+
+    // retrieving information from database location that corresponds to the specific uid
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    // awaiting snapShot object which represents the data
+    const snapShot = await userRef.get();
+    // if the snapShot object doesn't exist then we are taking the display name/email and creating a timestamp
+    // essentially if the data doesn't exist we create data in its place
+    // create a new user using the data from the userAuth object
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        //creating timestamp
+        const createdAt = new Date();
+        // creating a new user object with the specified items within the object
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+            // UNLESS it throws an error in which case we log it
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    // chances are we will want the userRef in our code
+    return userRef;
+};
+
 firebase.initializeApp(config);
 
 // exporting firebase
